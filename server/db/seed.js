3e1efd6742a55838c03604b975793c863a649296
si-mv <simon.haines@multiverse.io>
2023-01-27 20:34:54 +0000

    update schema

--- server/db/seed.js
+++ server/db/seed.js
@@ -10,20 +10,30 @@ async function seed () {
     email: 'em@il.com'
   })
 
-  await User.bulkCreate([
+  const users = await User.bulkCreate([
     { username: 'imran', email: 'hi@email.com' },
     { username: 'joe', email: 'hey@joe.com' }
   ])
 
-  const video = await Video.create({
-    description: 'All about JAVA!'
-  })
+  const videos = await Video.bulkCreate([
+    {
+      title: 'How To Build A Chat App With React And Stream',
+      url: 'https://www.youtube.com/watch?v=Een66E0X_os',
+      description: 'Building a chat application is hard, especially if you want it to be a robust and full featured app similar to Facebook Messenger. In this video I will show you how you can use Stream to build out an incredibly robust chat system with ease.'
+    },
+    {
+      title: 'Learn DOM Manipulation In 18 Minutes',
+      url: 'https://www.youtube.com/watch?v=y17RuWkWdn8',
+      description: 'DOM manipulation is tough. There are lots of methods and techniques you need to master and it is not obvious which methods are best for each scenario. In this video I cover the 14 most important DOM manipulation methods that you need to know.'
+    },
+    {
+      title: 'JavaScript Shopping Cart Tutorial for Beginners',
+      url: 'https://www.youtube.com/watch?v=YeFzkC2awTM&list=PLZlA0Gpn_vH9k5ju1yq9qCDqvtuTVgTr6',
+      description: 'In this video we will learn how to utilize JavaScript to add functionality to a shopping cart. We will cover how to check if the document is loaded, and how to query the document for elements by class, how to add events to elements. We will then combine all of these techniques to make the shopping cart in our web page work in an intuitive way.'
+    }
+  ])
 
-  const video2 = await Video.create({
-    description: 'All about C#!'
-  })
-
-  await user.addVideo(video) // <-- magic method added by sequelize
+  await users[1].addVideo(videos[2])
 
 }
 
--- server/models/Video.js
+++ server/models/Video.js
@@ -9,9 +9,24 @@ Video.init({
     defaultValue: DataTypes.UUIDV4,
     primaryKey: true
   },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  url: {
+    type: DataTypes.STRING
+  },
   description: {
     type: DataTypes.STRING,
     allowNull: false
+  },
+  liked: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  },
+  watched: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
   }
 }, { sequelize: db })
 
